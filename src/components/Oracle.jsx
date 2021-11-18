0c755511ef1de7b207847300c5446a54c7568dc8
onlyphantom <samuel@outlook.sg>
2021-11-18 19:49:15 +0700

    add recommendation checkbox

--- src/components/Oracle.jsx
+++ src/components/Oracle.jsx
@@ -1,74 +1,69 @@
-import React from "react";
-import { Input, Alert, Form, Button } from "antd";
+import React from 'react';
+import { Input, Alert, Form, Button, Checkbox } from 'antd';
 
 const Oracle = () => {
+  const [val, setVal] = React.useState({});
+  // const [api, setApi] = React.useState(null)
 
-    const [val, setVal] = React.useState({})
-    // const [api, setApi] = React.useState(null)
+  // React.useEffect(()=>{
+  //     fetch('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken')
+  //     .then(res => res.json())
+  //     .then(data => setVal(data.result))
+  // }, [])
 
-    // React.useEffect(()=>{
-    //     fetch('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken')
-    //     .then(res => res.json())
-    //     .then(data => setVal(data.result))
-    // }, [])
+  const formSubmit = (values) => {
+    console.log('values', values);
+    getApi(values.amount, values.apiKey);
+  };
 
-    const formSubmit = (values) => {
-        console.log("values", values)
-        getApi(values.amount, values.apiKey)
+  const getApi = (amount, apiKey) => {
+    fetch(
+      `https://api.etherscan.io/api?module=gastracker&action=gasoracle&amount=${amount}&apikey=${apiKey}`
+    );
+    const url_oracle = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle';
+
+    if (apiKey) {
+      console.log('Found apiKey. Using', apiKey);
+      url_oracle += `&apikey=${apiKey}`;
     }
 
-    const getApi = (amount, apiKey) => {
+    fetch(url_oracle)
+      .then((res) => res.json())
+      .then((data) => setVal(data.result));
+  };
 
-        const url = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle'
+  return (
+    <Form onFinish={formSubmit} layout="vertical" initialValues={{ recommendations: true }}>
+      <h2>Gas Oracle from Etherscan</h2>
+      <Form.Item name="apiKey" label="API Key">
+        <Input.Password
+          placeholder="Enter your Etherscan API key (Optional)"
+          style={{ width: '80%' }}
+        />
+      </Form.Item>
 
-        if(apiKey) {
-            console.log("Found apiKey. Using", apiKey)
-            url += `&apikey=${apiKey}`
-        }
+      <Form.Item
+        name="amount"
+        label="Amount"
+        style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
+      >
+        <Input addonBefore="Gas Price" placeholder="Price per unit of gas in wei" />
+      </Form.Item>
 
-        fetch(url)
-        .then(res => res.json())
-        .then(data => setVal(data.result))
-    }
+      <Form.Item name="recommendations" valuePropName="checked">
+        <Checkbox style={{ color: '#61a0e0' }}>Price Recommendations</Checkbox>
+      </Form.Item>
 
-    return (    
-        <Form
-            onFinish={formSubmit}
-            layout="vertical"
-        >
-            <h2>Gas Oracle from Etherscan</h2>
-            <Form.Item name="apiKey" label="API Key">
-                <Input.Password placeholder="Enter your Etherscan API key (Optional)" style={{width: '80%'}} />
-            </Form.Item>
-        
-            <Form.Item 
-                name="amount" 
-                label="Amount"
-                style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
-            >
-                <Input 
-                    addonBefore="ETH"
-                    placeholder="Amount of ETH To Transfer" 
-                />
-            </Form.Item>
-            <Form.Item
-                label=" "
-                style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '-4px 8px' }}
-            >
-                <Button type="primary" htmlType="submit">
-                    Get Estimate
-                </Button>
-            </Form.Item>
-          
-            <p style={{color: 'white'}}>
-                {JSON.stringify(val)} 
-            </p>
-            <Alert
-                description="⛽ The gas prices are returned in Gwei."
-                type="info"
-                showIcon />
-        </Form>
-    )
-}
+      <Form.Item label=" ">
+        <Button type="primary" htmlType="submit">
+          Get Estimate
+        </Button>
+      </Form.Item>
+
+      <p style={{ color: 'white' }}>{JSON.stringify(val)}</p>
+      <Alert description="⛽ The gas prices are returned in Gwei." type="info" showIcon />
+    </Form>
+  );
+};
 
 export default Oracle;
