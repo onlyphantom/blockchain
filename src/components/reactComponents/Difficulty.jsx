319711093909cba46b1e9a8cd5647e6d34ba8c0a
Samuel Chan <samuel@outlook.sg>
2022-01-06 14:13:31 +0800

    bitcoin difficulty chart

--- src/components/reactComponents/Difficulty.jsx
+++ src/components/reactComponents/Difficulty.jsx
@@ -1,6 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Chart as ChartJS } from 'chart.js/auto'
-import { Line } from "react-chartjs-2";
+import React, { useState, useEffect, useRef } from "react";
+import { Chart, getElementAtEvent } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, LineController, LineElement, PointElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, LineController, LineElement, PointElement, Title, Tooltip, Legend);
+
+const epochSecsToDate = (epochSecs) => {
+    let date = new Date(0);
+    date.setUTCSeconds(epochSecs);
+    return date.toLocaleDateString()
+}
 
 const data = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
@@ -21,10 +29,50 @@ const data = {
     ]
 };
 
+
 export default function Difficulty() {
+
+    const chartRef = useRef();
+
+    const [dat, setDat] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: "Mining Difficulty",
+                data: [],
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)"
+            },
+        ]
+    })
+
+    const onClick = (event) => {
+        console.log(getElementAtEvent(chartRef.current, event));
+    }
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/onlyphantom/blockchain/main/etc/difficulty.json")
+            .then(res => res.json())
+            .then(data => {
+                setDat(prev => {
+                    return {
+                        ...prev,
+                        labels: data.values.map(d => epochSecsToDate(d.x)),
+                        datasets: [
+                            {
+                                ...prev.datasets[0],
+                                data: data.values.map(d => d.y)
+                            }
+                        ]
+                    }
+                })
+            })
+    }, [])
+
     return (
-        <div className="App">
-            <Line data={data} />
+        <div className="chartjs-container">
+            <Chart type='line' data={dat} ref={chartRef} onClick={onClick} />
         </div>
     );
 }
