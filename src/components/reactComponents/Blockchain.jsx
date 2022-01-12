dfc7877ded0680cced6586822624aadad51353c7
Samuel Chan <samuel@outlook.sg>
2022-01-12 20:41:38 +0800

    add react component

---
+++ src/components/reactComponents/Blockchain.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Button, Space, Collapse, message } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+
+const Blockchain = () => {
+    return (
+        <div>
+            <h3>Visual Demo</h3>
+        </div>
+    )
+}
+
+export default Blockchain
